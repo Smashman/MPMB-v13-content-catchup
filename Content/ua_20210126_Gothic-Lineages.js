cc3bcdf289669a44759d3b61bed695665d5a4f75
Metacomet10 <metacomet10@me.com>
2021-01-26 22:16:45 -0800

    Gothic Lineages file

---
+++ Content/ua_20210126_Gothic-Lineages.js
@@ -0,0 +1,11 @@
+var iFileName = "ua_20210126_Gothic-Lineages.js";
+RequiredSheetVersion(13);
+
+// Define the source
+SourceList["UA:GL"] = {
+	name : "Unearthed Arcana: Gothic Lineages",
+	abbreviation : "UA:GL",
+	group : "Unearthed Arcana",
+	url : "https://media.wizards.com/2021/dnd/downloads/UA2021_GothicLineages.pdf",
+	date : "2021/01/26"
+};
